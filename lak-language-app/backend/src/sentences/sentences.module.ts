a7cfb84a7ca7cf6168cccfdf5baa612b99608365
Gamzat <gamzat@MacBook-Pro-Gamzat-2.local>
2026-04-21 16:42:44 +0300

    delete dir

--- lak-language-app/backend/src/sentences/sentences.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SentencesController } from './sentences.controller';
-
-@Module({
-  controllers: [SentencesController],
-})
-export class SentencesModule {}
--- lak-language-app/backend/src/words/words.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WordsController } from './words.controller';
-
-@Module({
-  controllers: [WordsController],
-})
-export class WordsModule {}
